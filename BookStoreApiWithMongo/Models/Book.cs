9630a29862d71218bd05350602e3e6505b74e9af
kadirkaradag <kadirkaradag0132@gmail.com>
2023-01-29 16:42:02 +0300

    Add project files.

---
+++ BookStoreApiWithMongo/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace BookStoreApiWithMongo.Models
+{
+    public class Book
+    {
+        [BsonId] //Annotated with [BsonId] to make this property the document's primary key.
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Name")]
+        [JsonPropertyName("Name")]
+        public string BookName { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; } = null!;
+
+        public string Author { get; set; } = null!;
+    }
+}
